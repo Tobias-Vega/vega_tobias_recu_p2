e721620a5d22364b07bb5be0cafe95fb8fdd2e3a
Tobias <tobiasvega1210@gmail.com>
2024-07-04 15:04:38 -0300

    Se realizó el metodo POST

--- app.js
+++ app.js
@@ -28,6 +28,40 @@ app.get('/students/:id', (req,res) => {
 })
 
 app.post('/students', (req,res) => {
+    const newId = new Date().getTime()
+
+    let { fullName, age, curse } = req.body
+
+    fullName = fullName.trim()
+    age = parseInt(age)
+    curse = curse.trim()
+
+    if(Number.isNaN(age)) {
+        return res.status(400).send('El año debe ser un número')
+    }
+
+    if(age < 0 || age < 6) {
+        return res.status(400).send('Ingrese un año válido para la edad de un estudiante')
+    }
+
+    if(!fullName || !age || !curse) {
+        return res.status(400).send('Faltan completar datos')
+    }
+
+    const repeatName = students.find((student) => student.fullName === fullName)
+
+    if(repeatName) {
+        return res.status(401).send('Ya existe un estudiante con dicho nombre')
+    }
+
+    students.push({
+        id: newId,
+        fullName: fullName,
+        age: age,
+        curse: curse
+    })
+
+    res.send('Datos del estudiante creados existosamente')
 
 })
 
